c5abbd77e760cc6c4f9f1f2db5a0f1abaf9114e0
Raj Sekhar Paul <rajsekharpaul45@gmail.com>
2024-09-23 13:22:12 +0530

    Added features to To-Do

--- to-do-list/src/TodoList.jsx
+++ to-do-list/src/TodoList.jsx
@@ -1,17 +1,51 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 export default function TodoList(){
-    let [todos,setTodos]=useState(["sample Task"]);
+    let [todos,setTodos]=useState([{task:"sample-task",id:uuidv4(), isDone:false}]);
     let [newTodo, setNewTodo] = useState("");
 
     let addNewTask = ()=>{
-        setTodos([...todos, newTodo]);
+        setTodos((prevTodos)=>{
+            return [...prevTodos, {task:newTodo, id: uuidv4(), isDone:false}]
+        });
         setNewTodo("");
     };
 
     let updateTodoValue = (event) => {
         setNewTodo(event.target.value);
     };
+
+    let deleteTodo = (id) =>{
+        setTodos((prevTodos) => todos.filter((prevTodos)=>prevTodos.id != id));
+    };
+
+    let MarkAllAsDone = () =>{
+        setTodos((prevtodos) => (
+            prevtodos.map((todo)=>{
+                return{
+                    ...todo,
+                    isDone:true
+                };
+            })
+        ));
+    };
+
+    let MarkOneAsDone=(id)=>{
+        setTodos(
+            (prevtodos) => (
+            prevtodos.map((todo)=>{
+                if(todo.id == id){
+                    return{
+                        ...todo,
+                        isDone:true,
+                    };
+                }else{
+                    return todo;
+                }
+            })
+        ));
+    }
 
     return(<div>
         <input onChange={updateTodoValue} value={newTodo} style={{ width: '200px', height: '30px', fontSize:'1.25rem' }} placeholder="add a task" ></input><br/><br/>
@@ -21,10 +55,17 @@ export default function TodoList(){
         <ul>
             {
                 todos.map((todo)=>(
-                    <li>{todo}</li>
-                ))
+                    <li key={todo.id}>
+                        <span style={todo.isDone?{textDecorationLine:'line-through'}:{}}>{todo.task}</span>
+                        &nbsp;&nbsp;&nbsp;
+                        <button onClick={() => deleteTodo(todo.id)} style={{ width: '75px', height: '30px', fontSize:'.75rem'}}>Delete</button>
+                        &nbsp;&nbsp;&nbsp;
+                        <button onClick={() => MarkOneAsDone(todo.id)} style={{ width: '120px', height: '30px', fontSize:'.75rem'}}>Mark as Done</button>
+                    </li>             
+                )) 
             }
         </ul>
+        <button onClick={MarkAllAsDone}>Mark All as Done</button>
     </div>
     );
 };
